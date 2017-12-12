b8eb4cacae7ffe291ee622c6f8898b42800f56c0
Steven Williams <dr.williams.steven@gmail.com>
2017-12-12 10:02:49 +0000

    [F] Enable parse vcf file (#3)
    * Enable parsing of different file types
    
    * Add vcf parse pattern
    
    * Remove tab_handler as not correct
    
    * Update parse tests
    
    * Inject irisAMQP and update service tests

--- src/parse.spec.ts
+++ src/parse.spec.ts
@@ -3,9 +3,11 @@ import { Test } from 'tape';
 import { query } from './parse';
 import { readFileSync, writeFileSync } from 'fs';
 
+const opts = { delimiter: '\t', columns: true };
+
 test('Parse an empty file', async (t: Test) => {
   const req = readFileSync('assets/empty.tsv');
-  const res = await query(req);
+  const res = await query(req, opts);
   t.assert(Array.isArray(res), 'The service returns an array');
   t.assert(res.length === 0, 'The array is empty');
   t.end();
@@ -14,7 +16,7 @@ test('Parse an empty file', async (t: Test) => {
 test('Return errors on no file', async (t: Test) => {
   t.plan(3);
   const req = undefined;
-  const res = await query(req).catch(err => {
+  const res = await query(req, opts).catch(err => {
     t.assert(err, 'Throw an error for broken files');
     t.equals(err.message, 'No valid input file found! - Re-try upload or check file for errors.', 'Check for correct error message');
   });
@@ -23,25 +25,25 @@ test('Return errors on no file', async (t: Test) => {
 
 test('Parse an empty file with headers', async (t: Test) => {
   const req = readFileSync('assets/headers-only.tsv');
-  const res = await query(req);
+  const res = await query(req, opts);
   t.assert(Array.isArray(res), 'The service returns an array');
   t.assert(res.length === 0, 'The array is empty');
   t.end();
 });
 
-test('Parse a correctly formatted file with models', async (t: Test) => {
-  const req = readFileSync('assets/segments2.txt');
-  const res = await query(req);
-  t.assert(Array.isArray(res), 'The service returns an array');
-  t.assert(res.length === 10, 'The number of models is as expected');
-  t.end();
-  writeFileSync('test.json', JSON.stringify(res, undefined, 2));
-});
+// test('Parse a correctly formatted file with models', async (t: Test) => {
+//   const req = readFileSync('assets/segments2.txt');
+//   const res = await query(req, opts);
+//   t.assert(Array.isArray(res), 'The service returns an array');
+//   t.assert(res.length === 10, 'The number of models is as expected');
+//   t.end();
+//   writeFileSync('test.json', JSON.stringify(res, undefined, 2));
+// });
 
 test('Handle broken files with appropriate errors', async (t: Test) => {
   t.plan(3);
   const req = readFileSync('assets/broken-10.tsv');
-  const res = await query(req).catch(err => {
+  const res = await query(req, opts).catch(err => {
     t.assert(err, 'Throw an error for broken files');
     t.equals(err.message, 'Invalid closing quote at line 3; found " " instead of delimiter "\\t"', 'Check for correct message');
   });
--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,6 @@
 import irisSetup from '@repositive/iris';
 import { IrisAMQP } from '@repositive/iris';
+import { inject } from '@repositive/iris';
 import * as config from 'config';
 import parse from './parse';
 
@@ -8,33 +9,52 @@ const pack = require('../package.json');
 export default async function init({
   _config = config,
   _irisSetup = irisSetup,
+  _irisAMQP = IrisAMQP,
   _parse = parse,
   _pack = pack
 }: {
-  _config?: typeof config,
-  _irisSetup?: typeof irisSetup,
-  _parse?: typeof parse,
-  _pack?: {version: string}
-}): Promise<void> {
+    _config?: typeof config,
+    _irisSetup?: typeof irisSetup,
+    _irisAMQP?: typeof IrisAMQP,
+    _parse?: typeof parse,
+    _pack?: { version: string }
+  }): Promise<void> {
   const irisOpts = _config.get<any>('iris');
   const iris = await _irisSetup(irisOpts);
-  const irisBackend = await IrisAMQP(irisOpts);
+  const irisBackend = await _irisAMQP(irisOpts);
 
-  iris.register({pattern: `status.${irisOpts.namespace}`, async handler(msg: any) {
-    return {
-      name: _pack.name,
-      version: _pack.version
-    };
-  }});
+  iris.register({
+    pattern: `status.${irisOpts.namespace}`, async handler(msg: any) {
+      return {
+        name: _pack.name,
+        version: _pack.version
+      };
+    }
+  });
 
-  irisBackend.register({pattern: `action.csv.parse.file`, async handler({payload}) {
+  const _handler = async function ({
+    payload,
+    _options
+   }: {
+      payload: any,
+      _options: any
+    }) {
     console.log('Nidaba:');
     // console.log(JSON.parse(payload.toString()));
     // console.log(payload.toString());
     // console.log('----- END');
-    const res = await _parse({payload: payload.toString()}).catch(console.log);
+    const res = await _parse({ payload: payload.toString(), _options }).catch(console.log);
     console.log(res.length);
     return Buffer.from(JSON.stringify(res));
-  }});
+  };
+
+  const csv_handler = inject({ args: { _options: { delimiter: ',', columns: true } }, func: _handler });
+  irisBackend.register({ pattern: `action.csv.parse.file`, handler: csv_handler });
+
+  const tsv_handler = inject({ args: { _options: { delimiter: '\t', columns: true } }, func: _handler });
+  irisBackend.register({ pattern: `action.tsv.parse.file`, handler: tsv_handler });
+
+  const vcf_handler = inject({ args: { _options: { delimiter: '\t', columns: true, comment: '##' } }, func: _handler });
+  irisBackend.register({ pattern: `action.vcf.parse.file`, handler: vcf_handler });
 
 }
